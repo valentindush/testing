d63ab6be868ee62d99f57cc78e43e6e646a4f4d7
Valentin <93974733+valentindush@users.noreply.github.com>
2022-11-05 21:59:07 +0200

    init

---
+++ functions.js
@@ -0,0 +1,9 @@
+const sum = (a,b)=>{
+    return a+b
+}
+
+const duplicateArray = (arr)=>{
+    return[...arr]
+}
+
+module.exports = {sum, duplicateArray}
---
+++ functions.test.js
@@ -0,0 +1,4 @@
+const {sum,duplicateArray} = require('./functions')
+test('sum of a + b', ()=>{
+    expect(sum(1,2)).toBe(3)
+})
